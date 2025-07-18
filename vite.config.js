8142cf66211f4edbfdec32d1add37afa5efdd73d
Max Heinze <maxheinze@googlemail.com>
2025-07-18 19:59:50 +0200

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/lonelydev/',
   server: {
     historyApiFallback: true, // ✅ allows SPA routing
   }
