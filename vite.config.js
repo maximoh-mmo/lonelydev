92e10337038ee039636f03861439f9402e1c037a
Max Heinze <maxheinze@googlemail.com>
2025-07-22 11:42:57 +0200

    copy index to 404

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs';
 
 export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
     historyApiFallback: true, // ✅ allows SPA routing
+  },
+  build: {
+    rollupOptions: {    
+    }
+  },
+  // After build, copy index.html to 404.html
+  buildEnd: () => {
+    const indexPath = resolve(__dirname, 'dist/index.html');
+    const notFoundPath = resolve(__dirname, 'dist/404.html');
+    if (fs.existsSync(indexPath)) {
+      fs.copyFileSync(indexPath, notFoundPath);
+    }
   }
-})
+});
