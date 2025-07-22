8f17a44777927297502b9fefdb311c88aa2378b9
Max Heinze <maxheinze@googlemail.com>
2025-07-22 10:20:38 +0200

    build: remove subpath from vite base config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lonelydev/',
+  base: '/',
   server: {
     historyApiFallback: true, // ✅ allows SPA routing
   }
